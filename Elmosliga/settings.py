86f128e20c3296fdc1dd0d1ff88a1af390153029
Amireza <Amirreza.Mehrani@gmail.com>
2025-12-31 14:52:11 +0330

    Update settings.py

--- Elmosliga/settings.py
+++ Elmosliga/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
+    "django-cors-headers",
     "accounts",
     "League",
     "rest_framework",
