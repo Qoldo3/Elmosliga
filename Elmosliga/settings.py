63f56b556bdf17ae2a93d547fee2e6e4b8acbee0
Amireza <Amirreza.Mehrani@gmail.com>
2025-12-31 13:10:49 +0330

    Update settings.py

--- Elmosliga/settings.py
+++ Elmosliga/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "Elmosliga.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": BASE_DIR / 'database/db.sqlite3',
+        "NAME": BASE_DIR / 'db.sqlite3',
     }
 }
 #test
